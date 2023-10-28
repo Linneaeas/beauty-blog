9c77bee9d9c70faf8174b199f4d802f828055e30
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-10-28 15:10:11 +0200

    Toggle display of new post BTN & design.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import {
+  ViewStateService,
+  ViewType,
+} from 'src/app/services/view-state.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  currentView!: ViewType;
+  Creator = ViewType.Creator;
+  User = ViewType.User;
 
+  constructor(private viewStateService: ViewStateService) {
+    this.viewStateService.currentView$.subscribe((view) => {
+      this.currentView = view;
+    });
+  }
 }
