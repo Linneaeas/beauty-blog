be9ced351e230ed1031b59b7a6abdca74edc0503
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-10-28 14:21:35 +0200

    Toggle View  & Subscribe to View-state service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ViewStateService, ViewType } from './services/view-state.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'beauty-blog';
+  currentView!: ViewType;
+
+  constructor(private viewStateService: ViewStateService) {
+    this.viewStateService.currentView$.subscribe((view) => {
+      this.currentView = view;
+    });
+  }
+
+  /*VIEW*/
+  toggleView() {
+    this.currentView =
+      this.currentView === ViewType.Creator ? ViewType.User : ViewType.Creator;
+    this.viewStateService.setCurrentView(this.currentView);
+  }
+  getViewButtonText(): string {
+    return this.currentView === 'user' ? 'User' : 'Creator';
+  }
 }
