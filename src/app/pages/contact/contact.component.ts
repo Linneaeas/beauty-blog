ec62a7fb0186205f343a7e0319f400f1fe02e8e6
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-11-01 09:55:20 +0100

    Contact form displays from the bottom up on scroll.

---
+++ src/app/pages/contact/contact.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css'],
+})
+export class ContactComponent {
+  /*CONTACT ME FORM*/
+  showFormFields: boolean = false;
+  sent: boolean = false;
+  yourName: string = '';
+  yourEmail: string = '';
+  yourMessage: string = '';
+  yourNumber: string = '';
+
+  onSubmit() {
+    console.log('Name:', this.yourName);
+
+    console.log('Email:', this.yourEmail);
+    console.log('Phone:', this.yourNumber);
+    console.log('Message:', this.yourMessage);
+
+    this.yourName = '';
+
+    this.yourEmail = '';
+    this.yourNumber = '';
+    this.yourMessage = '';
+    this.showFormFields = false;
+    this.sent = true;
+  }
+}
