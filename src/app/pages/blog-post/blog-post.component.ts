fb4c242ea32fce965d4ae2ae6d5f8208585704d1
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-11-17 10:52:50 +0100

    Displaying the post

--- src/app/pages/blog-post/blog-post.component.ts
+++ src/app/pages/blog-post/blog-post.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { ActivatedRoute } from '@angular/router';
+import { Post } from 'src/app/interfaces/post';
+import { Comment } from 'src/app/interfaces/comment';
+import {
+  ViewType,
+  ViewStateService,
+} from 'src/app/services/view-state.service';
 
 @Component({
   selector: 'app-blog-post',
   templateUrl: './blog-post.component.html',
-  styleUrls: ['./blog-post.component.css']
+  styleUrls: ['./blog-post.component.css'],
 })
 export class BlogPostComponent {
+  currentView!: ViewType;
+  Creator = ViewType.Creator;
+  User = ViewType.User;
+  post: Post = {
+    id: '',
+    title: '',
+    body: '',
+    tags: [],
+    thumbnailUrl: '',
+    creationDate: new Date(),
+    likes: 0,
+    dislikes: 0,
+    comments: [],
+  };
+  newComment: Comment = {
+    id: '',
+    text: '',
+    name: '',
+  };
 
+  constructor(
+    private viewStateService: ViewStateService,
+    private route: ActivatedRoute,
+    private localStorageService: LocalStorageService
+  ) {
+    this.viewStateService.currentView$.subscribe((view) => {
+      this.currentView = view;
+    });
+  }
+  getSavedPostData(): Post[] {
+    return this.localStorageService.get('posts');
+  }
+  ngOnInit() {
+    const postId = this.route.snapshot.paramMap.get('id');
+
+    const postData = this.getSavedPostData().find(
+      (post: any) => post.id === postId
+    );
+
+    if (postData) {
+      this.post = postData;
+    }
+    if (this.post) {
+      this.post.likes = this.post.likes || 0;
+      this.post.dislikes = this.post.dislikes || 0;
+    }
+  }
 }
